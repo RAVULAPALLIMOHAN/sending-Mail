53c8d1218615ecbfb775058f544e8a6c2b88473f
Mohan <66682632+RAVULAPALLIMOHAN@users.noreply.github.com>
2022-10-09 23:58:21 +0530

    New Project for sending mail

---
+++ mailClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sendingmails.Models
+{
+    public class mailClass
+    {
+        public string Body { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+    }
+}

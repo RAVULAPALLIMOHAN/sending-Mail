25e193941964474fc19f0941a3bfe0ee3d188de3
Mohan <66682632+RAVULAPALLIMOHAN@users.noreply.github.com>
2022-10-16 00:53:16 +0530

    dropdown bind

---
+++ sendingmails/Controllers/DropdownController.cs
@@ -0,0 +1,21 @@
+﻿using sendingmails.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sendingmails.Controllers
+{
+    public class DropdownController : Controller
+    {
+        // GET: Dropdown
+        MVCEntities1 DB = new MVCEntities1();
+        public ActionResult Index()
+        {
+            List<m_district_test> distList = DB.m_district_test.ToList();
+            ViewBag.Districts=new SelectList(distList,"sno","District");
+            return View();
+        }
+    }
+} 

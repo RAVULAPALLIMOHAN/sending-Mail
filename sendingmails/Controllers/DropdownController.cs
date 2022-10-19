ed46c72c80124bff63859f8d0234eff091304bd0
Mohan <66682632+RAVULAPALLIMOHAN@users.noreply.github.com>
2022-10-20 00:11:28 +0530

    dropdown binfing

--- sendingmails/Controllers/DropdownController.cs
+++ sendingmails/Controllers/DropdownController.cs
@@ -4,11 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace sendingmails.Controllers
 {
     public class DropdownController : Controller
     {
+        string conn1 = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+        
         // GET: Dropdown
         MVCEntities1 DB = new MVCEntities1();
         public ActionResult Index()
@@ -16,6 +21,31 @@ namespace sendingmails.Controllers
             List<m_district_test> distList = DB.m_district.ToList();
             ViewBag.Districts=new SelectList(distList,"sno","District");
             return View();
+        }
+        public ActionResult Dropdownlist()
+        {
+            string qry = "select * from [dbo].[m_city_test]";
+            var kjhg =connection(qry);
+            List<SelectListItem> list = new List<SelectListItem>();
+            foreach (DataRow dr in ViewBag.cities.Rows)
+            {
+                list.Add(new SelectListItem { Text = dr["City"].ToString(),Value = dr["sno"].ToString() });
+            }
+            ViewBag.city = list;
+            
+            return View();
+        }
+        public ActionResult connection(string qry)
+        {
+            SqlConnection con = new SqlConnection(conn1);
+            SqlCommand cmd = new SqlCommand(qry, con);
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            ViewBag.cities = ds.Tables[0];
+            con.Close();
+            return View(ds);
         }
     }
 } 
